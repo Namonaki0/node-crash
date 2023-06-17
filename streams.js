3ed5ece0548ff7c46c6dd48fef9c97a816b3918a
Andre <andreferreira@live.co.uk>
2023-06-18 00:14:14 +0100

    Global object - methods and properties, Node module system - import & export, File system core module, Creating streams for reading and writing large amounts of data

---
+++ streams.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const readStream = fs.createReadStream("./docs/file3.txt", {
+  encoding: "utf8",
+});
+
+const writeStream = fs.createWriteStream("./docs/file4.txt");
+
+// readStream.on("data", (chunk) => {
+//   console.log("--- NEW CHUNK ---");
+//   console.log(chunk);
+//   writeStream.write("\n NEW CHUNK \n");
+//   writeStream.write(chunk);
+// });
+
+//? piping
+readStream.pipe(writeStream);
