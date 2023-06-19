3b0a111e27dbb7b0bca01f5a6a00941936281e20
Andre <andreferreira@live.co.uk>
2023-06-19 11:35:23 +0100

    requests & responses

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const fs = require("fs");
+const _ = require("lodash");
+
+const server = http.createServer((req, res) => {
+  //? lodash
+  const num = _.random(0, 30);
+  console.log(num);
+
+  //?   set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  //?   matching paths
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  //?   send an html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+server.listen("3000", "localhost", () => {
+  console.log("listening to any requests");
+});
