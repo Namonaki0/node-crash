37ec9f6eb43db9dad2240a337e901506511d195f
Andre <andreferreira@live.co.uk>
2023-06-20 11:20:47 +0100

    express routing and dynamic content

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require("express");
 
+//? express app
 const app = express();
+
+//? register view engine
+app.set("view engine", "ejs");
 
 //? listen to requests
 app.listen("3000");
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  //? static
+  //   res.sendFile("./views/index.html", { root: __dirname });
+  //? dynamic
+  res.render("index");
 });
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  //? static
+  //   res.sendFile("./views/about.html", { root: __dirname });
+  //? dynamic
+  res.render("about");
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 //? redirects
@@ -19,5 +33,8 @@ app.get("/about-me", (req, res) => {
 
 //? 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  //? static
+  //   res.status(404).sendFile("./views/404.html", { root: __dirname });
+  //? dynamic
+  res.status(404).render("404");
 });
