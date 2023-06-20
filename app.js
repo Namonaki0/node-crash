2c07a62db542f98d9ec4eb912c08bfbc27f1630d
Andre <andreferreira@live.co.uk>
2023-06-20 11:49:49 +0100

    dynamic data rendering via routing

--- app.js
+++ app.js
@@ -13,17 +13,31 @@ app.get("/", (req, res) => {
   //? static
   //   res.sendFile("./views/index.html", { root: __dirname });
   //? dynamic
-  res.render("index");
+  const blogs = [
+    {
+      title: "First blog",
+      snippet: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      title: "Second blog",
+      snippet: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      title: "Third blog",
+      snippet: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 app.get("/about", (req, res) => {
   //? static
   //   res.sendFile("./views/about.html", { root: __dirname });
   //? dynamic
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create");
+  res.render("create", { title: "Create a new blog" });
 });
 
 //? redirects
@@ -36,5 +50,5 @@ app.use((req, res) => {
   //? static
   //   res.status(404).sendFile("./views/404.html", { root: __dirname });
   //? dynamic
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "404" });
 });
